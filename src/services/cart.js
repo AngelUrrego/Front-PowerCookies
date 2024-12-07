e4454957c52ceb5090a4aaaabd8ee3f10bee4afa
AngelUrrego <angelurrego2010@gmail.com>
2024-12-07 11:59:32 -0500

    Front-Cookies

--- src/services/cart.js
+++ src/services/cart.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_URL + "/api";
 
 export const getCart = async (userId) => {
   const response = await axios.get(`${API_URL}/cart/${userId}`);
